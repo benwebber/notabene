benwebber/notabene

commit fe46549c900da864d8a2b21e7434cd07edcb9008

Hide CLI internals

src/cli.rs
@@ -1,0 +1,68 @@
+use std::io::Result as IoResult;
+use std::path::PathBuf;
+
+use clap::builder::{Styles, ValueParser};
+use clap::{Arg, ArgGroup, Command, value_parser};
+
+use crate::rule::Rule;
+
+mod commands;
+mod renderer;
+
+pub fn main() -> IoResult<()> {
+    let matches = Command::new("nb")
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .styles(Styles::plain())
+        .subcommand(
+            Command::new("check")
+                .about("Check a changelog")
+                .arg(Arg::new("FILE").value_parser(value_parser!(PathBuf)))
+                .arg(
+                    Arg::new("output_format")
+                        .long("output-format")
+                        .value_parser(["full", "json", "jsonl", "short"])
+                        .default_value("short"),
+                ),
+        )
+        .subcommand(
+            Command::new("rule")
+                .about("Explain a rule")
+                .group(ArgGroup::new("rule").required(true).args(["RULE", "all"]))
+                .arg(
+                    Arg::new("RULE")
+                        .help("The rule code (e.g., E100)")
+                        .value_parser(ValueParser::new(parse_rule_code))
+                        .conflicts_with("all"),
+                )
+                .arg(
+                    Arg::new("all")
+                        .short('a')
+                        .long("all")
+                        .help("Explain all rules")
+                        .action(clap::ArgAction::SetTrue),
+                ),
+        )
+        .get_matches();
+    match matches.subcommand() {
+        Some(("check", submatches)) => commands::check(submatches),
+        Some(("rule", submatches)) => commands::rule(submatches),
+        _ => unreachable!(),
+    }
+}
+
+fn parse_rule_code(code: &str) -> Result<String, String> {
+    let code = code.to_uppercase();
+    if Rule::ALL.iter().any(|rule| rule.code() == code) {
+        Ok(code)
+    } else {
+        Err(format!(
+            "{}",
+            Rule::ALL
+                .iter()
+                .map(|rule| rule.code())
+                .collect::<Vec<&str>>()
+                .join(", ")
+        ))
+    }
+}

src/cli/commands.rs
@@ -1,0 +1,83 @@
+use std::collections::HashMap;
+use std::io::{self, Result, Write};
+use std::path::PathBuf;
+
+use clap::ArgMatches;
+
+use crate::parse_file;
+use crate::rule::Rule;
+use crate::span::Index;
+
+use super::renderer::{OutputFormat, render};
+
+pub fn check(matches: &ArgMatches) -> Result<()> {
+    let path = matches
+        .get_one::<PathBuf>("FILE")
+        .unwrap_or(&PathBuf::from("CHANGELOG.md"))
+        .clone();
+    let format: &String = matches.get_one("output_format").expect("default");
+    let output_format = match format.as_str() {
+        "short" => OutputFormat::Short,
+        "json" => OutputFormat::Json,
+        "jsonl" => OutputFormat::JsonLines,
+        "full" => OutputFormat::Full,
+        _ => unreachable!(),
+    };
+    let (_, diagnostics) = parse_file(&path).unwrap();
+    if diagnostics.is_empty() {
+        Ok(())
+    } else {
+        // TODO: How to avoid reading again to create index?
+        let Ok(content) = std::fs::read_to_string(&path) else {
+            todo!();
+        };
+        let index = Index::new(&content);
+        let mut output = io::stdout();
+        render(
+            &mut output,
+            diagnostics.as_slice(),
+            &content,
+            Some(&path),
+            &index,
+            output_format,
+        )
+    }
+}
+
+pub fn rule(matches: &ArgMatches) -> Result<()> {
+    let mut rules_by_code = HashMap::new();
+    for rule in Rule::ALL {
+        rules_by_code.insert(rule.code().to_string(), rule);
+    }
+    let mut output = io::stdout();
+    if matches.get_flag("all") {
+        for (i, rule) in Rule::ALL.iter().enumerate() {
+            if i > 0 {
+                writeln!(output)?;
+            }
+            write!(
+                output,
+                "# {}
+
+{}
+",
+                rule.code(),
+                rule.doc()
+            )?;
+        }
+    } else if let Some(code) = matches.get_one::<String>("RULE") {
+        let rule = rules_by_code.get(code).unwrap();
+        write!(
+            output,
+            "# {}
+
+{}
+",
+            rule.code(),
+            rule.doc()
+        )?
+    } else {
+        unreachable!()
+    }
+    Ok(())
+}

src/lib.rs
@@ -6,11 +6,11 @@ pub(crate) mod ir;
 pub(crate) mod linter;
 pub(crate) mod parser;
 pub(crate) mod profile;
-#[cfg(feature = "cli")]
-pub(crate) mod renderer;
 pub(crate) mod rule;
 
 pub mod changelog;
+#[cfg(feature = "cli")]
+pub mod cli;
 pub mod error;
 pub mod span;
 pub mod unist;
@@ -19,9 +19,6 @@ pub use changelog::Changelog;
 pub use diagnostic::Diagnostic;
 pub use error::{Error, ParseError};
 pub use rule::Rule;
-
-#[cfg(feature = "cli")]
-pub use renderer::{OutputFormat, render};
 
 /// Parse a changelog from a string.
 ///

src/main.rs
@@ -1,146 +1,12 @@
-use std::collections::HashMap;
-use std::io::{self, Write};
-use std::path::PathBuf;
 use std::process::ExitCode;
 
 #[cfg(feature = "cli")]
-use clap::builder::Styles;
-use clap::builder::ValueParser;
-#[cfg(feature = "cli")]
-use clap::{Arg, Command, builder::ArgGroup, value_parser};
-
-use notabene::span::Index;
-#[cfg(feature = "cli")]
-use notabene::{OutputFormat, Rule, parse_file, render};
-
-fn parse_rule_code(code: &str) -> Result<String, String> {
-    let code = code.to_uppercase();
-    if Rule::ALL.iter().any(|rule| rule.code() == code) {
-        Ok(code)
-    } else {
-        Err(format!(
-            "{}",
-            Rule::ALL
-                .iter()
-                .map(|rule| rule.code())
-                .collect::<Vec<&str>>()
-                .join(", ")
-        ))
-    }
-}
+use notabene::cli;
 
 #[cfg(feature = "cli")]
 fn main() -> std::process::ExitCode {
-    let mut rules_by_code = HashMap::new();
-    for rule in Rule::ALL {
-        rules_by_code.insert(rule.code().to_string(), rule);
-    }
-    let matches = Command::new("nb")
-        .arg_required_else_help(true)
-        .subcommand_required(true)
-        .styles(Styles::plain())
-        .subcommand(
-            Command::new("check")
-                .about("Check a changelog")
-                .arg(Arg::new("FILE").value_parser(value_parser!(PathBuf)))
-                .arg(
-                    Arg::new("output_format")
-                        .long("output-format")
-                        .value_parser(["full", "json", "jsonl", "short"])
-                        .default_value("short"),
-                ),
-        )
-        .subcommand(
-            Command::new("rule")
-                .about("Explain a rule")
-                .group(ArgGroup::new("rule").required(true).args(["RULE", "all"]))
-                .arg(
-                    Arg::new("RULE")
-                        .help("The rule code (e.g., E100)")
-                        .value_parser(ValueParser::new(parse_rule_code))
-                        .conflicts_with("all"),
-                )
-                .arg(
-                    Arg::new("all")
-                        .short('a')
-                        .long("all")
-                        .help("Explain all rules")
-                        .action(clap::ArgAction::SetTrue),
-                ),
-        )
-        .get_matches();
-    match matches.subcommand() {
-        Some(("check", submatches)) => {
-            let path = submatches
-                .get_one::<PathBuf>("FILE")
-                .unwrap_or(&PathBuf::from("CHANGELOG.md"))
-                .clone();
-            let format: &String = submatches.get_one("output_format").expect("default");
-            let output_format = match format.as_str() {
-                "short" => OutputFormat::Short,
-                "json" => OutputFormat::Json,
-                "jsonl" => OutputFormat::JsonLines,
-                "full" => OutputFormat::Full,
-                _ => unreachable!(),
-            };
-            let (_, diagnostics) = parse_file(&path).unwrap();
-            if diagnostics.is_empty() {
-                ExitCode::SUCCESS
-            } else {
-                // TODO: How to avoid reading again to create index?
-                let Ok(content) = std::fs::read_to_string(&path) else {
-                    todo!();
-                };
-                let index = Index::new(&content);
-                let mut output = io::stdout();
-                render(
-                    &mut output,
-                    diagnostics.as_slice(),
-                    &content,
-                    Some(&path),
-                    &index,
-                    output_format,
-                )
-                .unwrap();
-                ExitCode::FAILURE
-            }
-        }
-        Some(("rule", submatches)) => {
-            let mut output = io::stdout();
-            if submatches.get_flag("all") {
-                for (i, rule) in Rule::ALL.iter().enumerate() {
-                    if i > 0 {
-                        writeln!(output).unwrap();
-                    }
-                    write!(
-                        output,
-                        "# {}
-
-{}
-",
-                        rule.code(),
-                        rule.doc()
-                    )
-                    .unwrap();
-                }
-                ExitCode::SUCCESS
-            } else if let Some(code) = submatches.get_one::<String>("RULE") {
-                let rule = rules_by_code.get(code).unwrap();
-                write!(
-                    output,
-                    "# {}
-
-{}
-",
-                    rule.code(),
-                    rule.doc()
-                )
-                .unwrap();
-                ExitCode::SUCCESS
-            } else {
-                unreachable!()
-            }
-        }
-        _ => unreachable!(),
+    match cli::main() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(_) => ExitCode::FAILURE,
     }
 }

commit e4fee9bca25a13057216fea8f3050242d4e7f86d

Restore exit code
Fixes regression introduced in fe46549.

See-also: fe46549 (Hide CLI internals, 2025-09-10)

src/cli.rs
@@ -11,7 +11,7 @@ mod config;
 mod error;
 mod renderer;
 
-pub fn main() -> IoResult<()> {
+pub fn main() -> error::Result<()> {
     let matches = Command::new("nb")
         .arg_required_else_help(true)
         .subcommand_required(true)

src/cli/commands.rs
@@ -1,5 +1,5 @@
 use std::collections::{HashMap, HashSet};
-use std::io::{self, Result, Write};
+use std::io::{self, Write};
 use std::path::PathBuf;
 
 use clap::ArgMatches;
@@ -10,6 +10,7 @@ use crate::rule::Rule;
 use crate::span::Index;
 
 use super::config::{Config, Lint};
+use super::error::{Error, Result};
 use super::renderer::{OutputFormat, render};
 
 pub fn check(matches: &ArgMatches) -> Result<()> {
@@ -64,7 +65,8 @@ pub fn check(matches: &ArgMatches) -> Result<()> {
             &index,
             // TODO: Build final config.
             config.lint.output_format.unwrap(),
-        )
+        )?;
+        Err(Error::Check)
     }
 }
 

src/cli/error.rs
@@ -9,6 +9,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     Io(IoError),
     Toml(TomlDeError),
+    Check,
 }
 
 impl std::error::Error for Error {}
@@ -18,6 +19,7 @@ impl fmt::Display for Error {
         match self {
             Self::Io(e) => write!(f, "I/O error: {}", e),
             Self::Toml(e) => write!(f, "TOML parse error: {}", e),
+            Self::Check => write!(f, "checks failed"),
         }
     }
 }